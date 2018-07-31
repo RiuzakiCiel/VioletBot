614837e17c27e4ee39c1b88dcd39ece43f923190
Ivar <ivar.scholten@protonmail.com>
2018-07-31 11:15:29 +0200

    Bot token via text file
    Authenticates using bot_token.txt instead of a hardcoded string in the code.

--- source/main.py
+++ source/main.py
@@ -1,10 +1,15 @@
 import discord
 import asyncio
+import os
 
 client = discord.Client()
 
-MsgAuthor = None
-
+if not os.path.isfile("bot_token.txt"):
+    print("Please insert your bot token")
+    token = input()
+    token_txt = open(r"bot_token.txt", "a+")
+    token_txt.write(token)
+    token_txt.close
 
 @client.event
 async def on_ready():
@@ -16,8 +21,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith(';;test'):
-        await client.send_message(message.channel, 'fuck off neko')
+        await client.send_message(message.channel, 'test')
         print("Someone used the ';;test' command")
 
-
-client.run('')
+token_txt = open(r"bot_token.txt", "r")
+token = token_txt.read()
+client.run(token)
+token_txt.close
