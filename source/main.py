f9b3db4401072861e3a687e2247e5ed2e48ee444
Ivar <ivar.scholten@protonmail.com>
2018-08-03 18:23:06 +0200

    Rewritten how the bot works.
    Uses `discord.ext.commands` instead of `discord.client` now

--- source/main.py
+++ source/main.py
@@ -2,10 +2,9 @@ import discord
 import asyncio
 import os
 import random
-import sys
 from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=";;")
 
 if not os.path.isfile("bot_token.txt"): #Authentication stuff
     print("Please insert your bot token")
@@ -14,69 +13,83 @@ if not os.path.isfile("bot_token.txt"): #Authentication stuff
     token_txt.write(token)
     token_txt.close
 
-@client.event
-async def on_ready(): #Executes the code below once the bot connects
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.event
+@bot.event
 async def on_member_join(member): #Welcome message
     server = member.server
     fmt = 'Hey {0.mention}, welcome to the {1.name}!\nPlease read the rules and have fun!'
-    await client.send_message(discord.Object(id='458347412910768128'), fmt.format(member, server))
+    await bot.send_message(discord.Object(id='458347412910768128'), fmt.format(member, server))
     print(member, "joined the the", server)
 
-@client.event
-async def on_message(message): #All the messages the bot sends
-    if message.content.startswith(';;r1'):
-        await client.send_message(message.channel, 'Do not impersonate people. You can swear in this server but do not use any racial/sexist/etc. (you know the drill) words.')
-        print(message.author, "used the ;;r1 command in the", message.channel, "channel")
-    if message.content.startswith(';;r2'):
-        await client.send_message(message.channel, "Keep the channels on topic. this applies to voice channels too!")
-        print(message.author, "used the ;;r2 command in the", message.channel, "channel")
-    if message.content.startswith(';;r3'):
-        await client.send_message(message.channel, "Read the description of the chat you're in to make sure you are not breaking any rules there, we are not super strict and let a lot just slip by, but you don't want to have the chance to be kicked. This applies to voice channels too!")
-        print(message.author, "used the ;;r3 command in the", message.channel, "channel")
-    if message.content.startswith(';;r4'):
-        await client.send_message(message.channel, "We do not tolerate useless @'s or @'s to random people. Do not tag mods unless it's really necessary!")
-        print(message.author, "used the ;;r4 command in the", message.channel, "channel")
-    if message.content.startswith(';;r5'):
-        await client.send_message(message.channel, "Do not tease people with their spelling of words, only if they really ask for help.")
-        print(message.author, "used the ;;r5 command in the", message.channel, "channel")
-    if message.content.startswith(';;r6'):
-        await client.send_message(message.channel, "DO NOT SPAM!!!!! Spamming is one of the easiest ways to get a warning/ban.")
-        print(message.author, "used the ;;r6 command in the", message.channel, "channel")
-    if message.content.startswith(';;r7'):
-        await client.send_message(message.channel, "Don't do drama. it's as simple as that.")
-        print(message.author, "used the ;;r7 command in the", message.channel, "channel")
-    if message.content.startswith(';;r8'):
-        await client.send_message(message.channel, "This is an english speaking server, don't randomly start talking in Spanish!")
-        print(message.author, "used the ;;r8 command in the", message.channel, "channel")
-    if message.content.startswith(';;taco'):
-        await client.send_message(message.channel, "Do you like taco's?")
-        print(message.author, "used the ;;taco command in the", message.channel, "channel")
-    if message.content.startswith(';;naru'):
-        RNG = random.randint(0, 2)
-        if RNG == 0:
-            await client.send_message(message.channel, "Cus itsa  SMALL DICK BOY")
-        if RNG == 1:
-            await client.send_message(message.channel, "The creepy thing?")
-        if RNG == 2:
-            await client.send_message(message.channel, "'I need yvar to wake tf up for this'\nNo you don't. Leave me the fuck alone")
-        print(message.author, "used the ;;naru command in the", message.channel, "channel")
-    if message.content.startswith(';;gny'):
-        RNG = random.randint(0, 2)
-        if RNG == 0:
-            await client.send_message(message.channel, "Ya shaveing smooth and my crack hard to do")
-        if RNG == 1:
-            await client.send_message(message.channel, "Ok hugs mmm u not still u know thinking ur bad right")
-        if RNG == 2:
-            await client.send_message(message.channel, "Clenches butt cheeks so poop doesn't come out")
-        print(message.author, "used the ;;gny command in the", message.channel, "channel")
+@bot.event #Startup message for host
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command(pass_context=True) 
+async def r1(ctx): #R1 command
+    await bot.say("Do not impersonate people. You can swear in this server but do not use any racial/sexist/etc. (you know the drill) words.")
+    print(ctx.message.author, "used the r1 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r2(ctx): #R2 command
+    await bot.say("Keep the channels on topic. this applies to voice channels too!")
+    print(ctx.message.author, "used the r2 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r3(ctx): #R3 command
+    await bot.say("Read the description of the chat you're in to make sure you are not breaking any rules there, we are not super strict and let a lot just slip by, but you don't want to have the chance to be kicked. This applies to voice channels too!")
+    print(ctx.message.author, "used the r3 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r4(ctx): #R4 command
+    await bot.say("We do not tolerate useless @'s or @'s to random people. Do not tag mods unless it's really necessary!")
+    print(ctx.message.author, "used the r4 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r5(ctx): #R5 command
+    await bot.say("Do not tease people with their spelling of words, only if they really ask for help.")
+    print(ctx.message.author, "used the r5 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r6(ctx): #R6 command
+    await bot.say("Do not spam. Spamming is one of the easiest ways to get a warning/ban.")
+    print(ctx.message.author, "used the r6 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r7(ctx): #R7 command
+    await bot.say("Don't do drama. it's as simple as that.")
+    print(ctx.message.author, "used the r7 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def r8(ctx): #R8 command
+    await bot.say("This is an english speaking server, so keep communicating to that language.")
+    print(ctx.message.author, "used the r8 command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def naru(ctx): #Naru command
+    RNG = random.randint(0, 2)
+    if RNG == 0:
+        await bot.say("Cus itsa  SMALL DICK BOY")
+    if RNG == 1:
+        await bot.say("The creepy thing?")
+    if RNG == 2:
+        await bot.say("'I need yvar to wake tf up for this'\nNo you don't. Leave me the fuck alone")
+    print(ctx.message.author, "used the naru command in the", ctx.message.channel, "channel")
+
+@bot.command(pass_context=True)
+async def gny(ctx): #Gny command
+    RNG = random.randint(0, 2)
+    if RNG == 0:
+        await bot.say("Ya shaveing smooth and my crack hard to do")
+    if RNG == 1:
+        await bot.say("Ok hugs mmm u not still u know thinking ur bad right")
+    if RNG == 2:
+        await bot.say("Clenches butt cheeks so poop doesn't come out")
+    print(ctx.message.author, "used the naru command in the", ctx.message.channel, "channel")
 
 token_txt = open(r"bot_token.txt", "r")
 token = token_txt.read()
-client.run(token)
+bot.run(token)
 token_txt.close
