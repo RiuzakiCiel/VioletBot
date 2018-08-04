008f66e01691ab883c6f8192ecc0f77d91143099
Ivar <ivar.scholten@protonmail.com>
2018-08-04 20:41:44 +0200

    Do more stuff in txt files

--- source/main.py
+++ source/main.py
@@ -7,17 +7,17 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix=";;")
 
-if not os.path.isfile("bot_token.txt"): #Authentication stuff
+if not os.path.isfile("../txt_files/bot_token.txt"): #Authentication stuff
     print("Please insert your bot token")
     token = input()
-    token_txt = open(r"bot_token.txt", "a+")
+    token_txt = open(r"../txt_files/bot_token.txt", "a+")
     token_txt.write(token)
     token_txt.close
 
 @bot.event
 async def on_member_join(member): #Welcome message
     server = member.server
-    fmt = 'Hey {0.mention}, welcome to the {1.name}!\nPlease read the rules and have fun!'
+    fmt = linecache.getline("../txt_files/general.txt", 1)
     await bot.send_message(discord.Object(id='458347412910768128'), fmt.format(member, server))
     print(member, "joined the the", server)
 
@@ -30,89 +30,89 @@ async def on_ready():
 
 @bot.command(pass_context=True) 
 async def r1(ctx): #R1 command
-    await bot.say(linecache.getline("rules.txt", 1))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 1))
     print(ctx.message.author, "used the r1 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r2(ctx): #R2 command
-    await bot.say(linecache.getline("rules.txt", 2))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 2))
     print(ctx.message.author, "used the r2 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r3(ctx): #R3 command
-    await bot.say(linecache.getline("rules.txt", 3))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 3))
     print(ctx.message.author, "used the r3 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r4(ctx): #R4 command
-    await bot.say(linecache.getline("rules.txt", 4))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 4))
     print(ctx.message.author, "used the r4 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r5(ctx): #R5 command
-    await bot.say(linecache.getline("rules.txt", 5))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 5))
     print(ctx.message.author, "used the r5 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r6(ctx): #R6 command
-    await bot.say(linecache.getline("rules.txt", 6))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 6))
     print(ctx.message.author, "used the r6 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r7(ctx): #R7 command
-    await bot.say(linecache.getline("rules.txt", 7))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 7))
     print(ctx.message.author, "used the r7 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def r8(ctx): #R8 command
-    await bot.say(linecache.getline("rules.txt", 8))
+    await bot.say(linecache.getline("../txt_files/rules.txt", 8))
     print(ctx.message.author, "used the r8 command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def naru(ctx): #Naru command
     RNG = random.randint(0, 2)
     if RNG == 0:
-        await bot.say("Cus itsa  SMALL DICK BOY")
+        await bot.say(linecache.getline("../txt_files/general.txt", 2))
     if RNG == 1:
-        await bot.say("The creepy thing?")
+        await bot.say(linecache.getline("../txt_files/general.txt", 3))
     if RNG == 2:
-        await bot.say("'I need yvar to wake tf up for this'\nNo you don't. Leave me the fuck alone")
+        await bot.say(linecache.getline("../txt_files/general.txt", 4))
     print(ctx.message.author, "used the naru command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def gny(ctx): #Gny command
     RNG = random.randint(0, 2)
     if RNG == 0:
-        await bot.say("Ya shaveing smooth and my crack hard to do")
+        await bot.say(linecache.getline("../txt_files/general.txt", 5))
     if RNG == 1:
-        await bot.say("Ok hugs mmm u not still u know thinking ur bad right")
+        await bot.say(linecache.getline("../txt_files/general.txt", 6))
     if RNG == 2:
-        await bot.say("Clenches butt cheeks so poop doesn't come out")
-    print(ctx.message.author, "used the naru command in the", ctx.message.channel, "channel")
+        await bot.say(linecache.getline("../txt_files/general.txt", 7))
+    print(ctx.message.author, "used the gny command in the", ctx.message.channel, "channel")
 
 @bot.command(pass_context=True)
 async def ban(ctx, member: discord.Member):
     if ctx.message.author.server_permissions.administrator:
         await bot.ban(member)
-        fmt = "{0.mention} is now banned!"
+        fmt = linecache.getline("../txt_files/general.txt", 8)
         await bot.send_message(ctx.message.channel, fmt.format(member))
         print(ctx.message.author, "banned", member)
     else:
-        await bot.say("You don't have permissions to use this command!")
+        await bot.say(linecache.getline("../txt_files/general.txt", 10))
         print(ctx.message.author, "tried to ban", member, "without permissions!")
 
 @bot.command(pass_context=True)
 async def kick(ctx, member: discord.Member):
     if ctx.message.author.server_permissions.administrator:
         await bot.kick(member)
-        fmt = "{0.mention} is now kicked"
+        fmt = linecache.getline("../txt_files/general.txt", 9)
         await bot.send_message(ctx.message.channel, fmt.format(member))
         print(ctx.message.author, "kicked", member)
     else:
-        await bot.say("You don't have permissions to use this command!")
+        await bot.say(linecache.getline("../txt_files/general.txt", 10))
         print(ctx.message.author, "tried to kick", member, "without permissions!")
 
-token_txt = open(r"bot_token.txt", "r")
+token_txt = open(r"../txt_files/bot_token.txt", "r")
 token = token_txt.read()
 bot.run(token)
 token_txt.close
